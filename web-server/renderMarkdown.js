e778944cab4712cc12e89c0e8b03b343641342d8
Jared Grippe <jared@deadlyicon.com>
2017-06-16 23:16:23 -0700

    WIP

--- web-server/renderMarkdown.js
+++ web-server/renderMarkdown.js
@@ -1,22 +1,39 @@
 const marked = require('marked')
-const highlight = require('highlight.js')
+const highlightjs = require('highlight.js')
 // const pygmentizeBundled = require('pygmentize-bundled')
 // const highlight = require('pygments').colorize;
 const pygmentize = require('pygmentize-bundled')
 
+const renderer = new marked.Renderer();
+
+renderer.code = (code, language) => {
+  // Check whether the given language is valid for highlight.js.
+  const validLang = !!(language && highlightjs.getLanguage(language));
+  // Highlight only if the language is valid.
+  const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+  // Render the highlighted code with `hljs` class.
+  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+};
+
 
 marked.setOptions({
-  renderer: new marked.Renderer(),
+  renderer: renderer,
   gfm: true,
   tables: true,
   breaks: false,
   pedantic: false,
   sanitize: true,
   smartLists: true,
+  // highlight: function (code, language) {
+  //   // Check whether the given language is valid for highlight.js.
+  //   const validLang = !!(language && highlightjs.getLanguage(language));
+  //   // Highlight only if the language is valid.
+  //   const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+  //   // Render the highlighted code with `hljs` class.
+  //   return `<code class="hljs ${language}">${highlighted}</code>`;
 
-  highlight: function (code) {
-    return highlight.highlightAuto(code).value;
-  }
+  //   // return highlightjs.highlightAuto(code).value;
+  // }
 
   // highlight: function (code, lang, callback) {
   //   console.log('CODE?', result.toString())
