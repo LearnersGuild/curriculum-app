f937f9a97a91f7a73c35daaaeb87ca3750b2960a
Jared Grippe <jared@deadlyicon.com>
2017-06-19 14:19:02 -0700

    added more reporting to the digest

--- digest/report.js
+++ digest/report.js
@@ -4,6 +4,10 @@ module.exports = digest => {
   const report = digest.report = {
     missingModules: [],
     missingSkills: [],
+    skillsThatSuck: [],
+    skillsThatAreShort: [],
+    skillsThatHaveFormattingErrors: [],
+    skillsMissingContext: [],
   }
 
   const getModuleById = moduleId =>
@@ -17,6 +21,31 @@ module.exports = digest => {
     })
   })
 
+  utils.values(digest.skills).forEach(skill => {
+    if (
+      skill.name.match(/^can /)
+    ){
+      report.skillsThatSuck.push(skill.rawText)
+    }
+
+    if (
+      skill.name.split(/\s+/).length < 4
+    ){
+      report.skillsThatAreShort.push(skill.rawText)
+    }
+
+    if (
+      skill.name.match(/\W(javascript|express|node|git)\W/)
+    ){
+      report.skillsThatHaveFormattingErrors.push(skill.rawText)
+    }
+
+    if (
+      !skill.name.match(/JavaScript|Node|Browser/)
+    ){
+      report.skillsMissingContext.push(skill.rawText)
+    }
+  })
 
   return digest
 }
