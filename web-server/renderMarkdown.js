d2d9b8792826cde386f5aa29c46177b6de20c149
Jared Grippe <jared@deadlyicon.com>
2017-06-19 11:34:06 -0700

    fixed undefined classname bug in code highlighter

--- web-server/renderMarkdown.js
+++ web-server/renderMarkdown.js
@@ -12,7 +12,7 @@ renderer.code = (code, language) => {
   // Highlight only if the language is valid.
   const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
   // Render the highlighted code with `hljs` class.
-  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+  return `<pre><code class="hljs ${language||''}">${highlighted}</code></pre>`;
 };
 
 renderer.listitem = function(text) {
