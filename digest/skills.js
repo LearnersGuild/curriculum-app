b4bebb8f7a096f8625c49f482fa38d3bc953e5bb
Jared Grippe <jared@deadlyicon.com>
2017-07-12 10:55:31 -0700

    added a test preventing two skills with the same ID from having a different name or rawText

--- digest/skills.js
+++ digest/skills.js
@@ -7,13 +7,34 @@ module.exports = digest => {
   const getSkillByRawText = rawText => {
     const name = utils.rawTextToName(rawText)
     const id = utils.nameToId(name)
-    return digest.skills[id] = digest.skills[id] || {
+    const newSkill = {
       id,
       name,
       rawText,
       modules: [],
       path: `/skills/${encodeURIComponent(id)}`,
     }
+    const preExistingSkill = digest.skills[id]
+    if (preExistingSkill){
+      assertIdenticalSkills(preExistingSkill, newSkill)
+      return preExistingSkill
+    }
+    digest.skills[id] = newSkill
+    return newSkill
+  }
+
+  const assertIdenticalSkills = (preExistingSkill, newSkill) => {
+    ['name','rawText'].forEach(key => {
+      if (preExistingSkill[key] !== newSkill[key]){
+        throw new Error(
+          `Skill MissMatch Error for skill ${newSkill.id}\n`+
+          `skill.${key} did not match\n`+
+          `expected: ${JSON.stringify(preExistingSkill[key])}\n`+
+          `recieved: ${JSON.stringify(newSkill[key])}\n`
+        )
+      }
+    })
+
   }
 
   utils.values(digest.modules).forEach(module => {
