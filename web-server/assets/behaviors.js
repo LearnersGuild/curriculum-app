629673d49b176cdae812e923861eba93c0867370
Jared Grippe <jared@deadlyicon.com>
2017-07-18 15:22:13 -0700

    improved the icons for practice and benchmark modules

--- web-server/assets/behaviors.js
+++ web-server/assets/behaviors.js
@@ -1,4 +1,18 @@
-(()=>{
+$(()=>{
+  $('h2#modules + ul').addClass('phase-modules-list')
+  $('em:contains("P:")')
+    .replaceWith(
+      $('<i class="practice-module-icon glyphicon glyphicon-minus"></i>')
+      .attr('title', 'Practive Module')
+    )
+  $('em:contains("B:")')
+    .replaceWith(
+      $('<i class="benchmark-module-icon glyphicon glyphicon-pushpin"></i>')
+      .attr('title', 'Benchmark Module')
+    )
+})
+
+;(()=>{
 
   const onModulePage = location.pathname.match(/^\/modules\//)
 
