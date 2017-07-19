0dc2efed8fea3c9374303ca3754f07562deb994b
Jared Grippe <jared@deadlyicon.com>
2017-07-19 14:56:46 -0700

    removed uneeded require from api routes

--- web-server/routes/api.js
+++ web-server/routes/api.js
@@ -2,7 +2,6 @@ const url = require('url')
 const bodyParser = require('body-parser')
 const queries = require('../../database/queries')
 const commands = require('../../database/commands')
-const goalsById = require('../../goals/goals')
 
 module.exports = app => {
 
