467247a09585171c49b6fbfaa59185f8029d9805
Jared Grippe <jared@deadlyicon.com>
2017-07-19 09:42:38 -0700

    added newrelic

--- environment.js
+++ environment.js
@@ -1,5 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+if (process.env.NODE_ENV === 'production'){
+  require('newrelic')
+}
+
 if (process.env.NODE_ENV ==='development'){
   require('dotenv').load()
 }
