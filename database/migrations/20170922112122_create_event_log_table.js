46e49f062fa75acd1f9885350a8637ec45b4fe65
je-lopez <je.lopez@me.com>
2017-09-23 17:39:40 -0700

    adds db util function to run after event_logs table creation

--- database/migrations/20170922112122_create_event_log_table.js
+++ database/migrations/20170922112122_create_event_log_table.js
@@ -1,12 +1,26 @@
+const databaseUtils = require('../utils')
+
 exports.up = knex =>
   Promise.all([
     knex.schema.createTable('event_log', table => {
       table.timestamp('occurred_at').defaultTo(knex.fn.now())
       table.string('user_id').notNullable().index()
       table.jsonb('metadata').notNullable()
-    })
+    }),
+
+    knex.schema.createTable('event_logs', table => {
+      table.timestamp('occurred_at').defaultTo(knex.fn.now())
+      table.string('user_id').notNullable()
+      table.string('type').notNullable()
+    }),
   ])
+  .then(tables =>
+    databaseUtils.migrateCheckLogs(knex)
+  )
 
 
 exports.down = knex =>
-  knex.schema.dropTable('event_log')
+  Promise.all([
+    knex.schema.dropTable('event_log'),
+    knex.schema.dropTable('event_logs'),
+  ])
