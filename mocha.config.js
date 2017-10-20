e49d7cf200174f496400fade95069dbb57d91098
Jared Grippe <jared@deadlyicon.com>
2017-10-20 11:52:23 -0700

    WIP

--- mocha.config.js
+++ mocha.config.js
@@ -3,7 +3,11 @@ process.env.NODE_ENV = 'test'
 process.env.CURRICULUM_BASE_URL = 'http://curriculum.learnersguild.test'
 process.env.IDM_BASE_URL = 'http://idm.learnersguild.test'
 process.env.ECHO_BASE_URL = 'http://echo.learnersguild.test'
-process.env.DATABASE_URL = 'postgres://localhost:5432/lg-curriculum-test'
+process.env.DATABASE_URL = (
+  process.env.POSTGRES_USER
+   ? 'postgres://127.0.0.1:5432/lg-curriculum-test'
+   : 'postgres://localhost:5432/lg-curriculum-test'
+)
 process.env.HUBSPOT_API_KEY = 'FAKE_HUBSPOST_API_KEY'
 process.env.JWT_PUBLIC_KEY = 'FAKE_PUBLIC_KEY'
 
