9eeab9b14fdddaf800d1ae80847e6fc6f9006edf
Punit Rathore <punitr@gmail.com>
2017-11-28 09:07:12 -0800

    turns out we need ssl after all

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,17 @@ require('./environment')
 const environments = 'development test staging production'.split(' ')
 const config = {}
 
+let databaseUrl
+if(process.env.DATABASE_URL) {
+  databaseUrl = process.env.DATABASE_URL + '?ssl=true'
+} else {
+  databaseUrl = process.env.DATABASE_URL
+}
+
 environments.forEach(env => {
   config[env] = {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: databaseUrl,
     migrations: {
       directory: __dirname + '/database/migrations',
       tableName: 'migrations'
